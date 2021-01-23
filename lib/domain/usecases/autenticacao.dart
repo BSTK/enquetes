51217fb478948fb15cf60763be5c3d0275a2ee27
Bruno Luz <bsilva.se@gmail.com>
2021-01-23 19:51:00 -0300

    main - Refatorando parametros de autenticação

--- lib/domain/usecases/autenticacao.dart
+++ lib/domain/usecases/autenticacao.dart
@@ -4,8 +4,18 @@ import 'package:meta/meta.dart';
 abstract class Autenticacao {
 
   Future<ContaAutenticacao> autenticar({
-    @required final String email,
-    @required final String senha
+    @required final AutenticacaoParams params
+  });
+
+}
+
+class AutenticacaoParams {
+  final String email;
+  final String senha;
+
+  const AutenticacaoParams({
+    @required this.email,
+    @required this.senha
   });
 
 }
