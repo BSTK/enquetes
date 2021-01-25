56985da2985d8a76019da450f01f506279312f37
Bruno Luz <bsilva.se@gmail.com>
2021-01-24 22:09:48 -0300

    main - Iniciando a tela de login

---
+++ lib/main/main.dart
@@ -0,0 +1,6 @@
+import 'package:enquetes/ui/components/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
---
+++ lib/ui/components/app.dart
@@ -0,0 +1,17 @@
+import 'package:enquetes/ui/pages/pages.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Enquetes',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
