34adf85cd820b5247afb3c82a5439c64658b9ecc
Bruno Luz <bsilva.se@gmail.com>
2021-01-26 23:22:21 -0300

    main - Adicionando documentação do LoginPage

---
+++ docs/features/login/login_page.md
@@ -0,0 +1,8 @@
+# Login Page
+
+> ## Regras de validação
+1. Os campos devem começar sem exibir uma mensagem de erro
+2.
+3.
+4.
+5.
--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-class LoginPresenterSpy extends Mock implements LoginPresenter { }
+class LoginPresenterSpy extends Mock implements LoginPresenter {}
 
 void main() {
 
