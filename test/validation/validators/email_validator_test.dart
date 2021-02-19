aba5d0ba52eb933026a000cc6828feb034b344b4
Bruno Luz <bsilva.se@gmail.com>
2021-02-19 20:39:59 -0300

    main - Adicionando caso de teste de EmailValidator

--- test/validation/validators/email_validator_test.dart
+++ test/validation/validators/email_validator_test.dart
@@ -9,14 +9,17 @@ class EmailValidator implements FieldValidation {
 
   @override
   String validate(final String valor) {
-    return null;
+    final regex = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final emailNuloVazio = (valor == null || valor.trim().isEmpty);
+    final valido = emailNuloVazio || regex.hasMatch(valor);
+    return valido ? null : 'Campo Obrigatório!';
   }
 
 }
 
 void main () {
 
-  test('Test - Deve retornar null quando o campo estiver (Vazio | Só Espaço | Nulo)', () {
+  test('Test - Deve retornar null quando o email estiver (Vazio | Só Espaço | Nulo)', () {
     final sut = EmailValidator(field: 'campo_email');
     final errorVazio = sut.validate('');
     final errorEspaco = sut.validate('  ');
@@ -25,6 +28,20 @@ void main () {
     expect(errorVazio, null);
     expect(errorEspaco, null);
     expect(errorNulo, null);
+  });
+
+  test('Test - Deve retornar null quando for passado um email valido', () {
+    final sut = EmailValidator(field: 'campo_email');
+    final error = sut.validate('bsilva.se@gmail.com');
+    expect(error, null);
+  });
+
+  test('Test - Deve retornar erro quando for passado um email inválido', () {
+    final sut = EmailValidator(field: 'campo_email');
+    final errorSemArroba = sut.validate('meuemail.contatogmail');
+    final errorSemDominio = sut.validate('meuemail.contato@');
+    expect(errorSemArroba, 'Campo Obrigatório!');
+    expect(errorSemDominio, 'Campo Obrigatório!');
   });
 
 }
