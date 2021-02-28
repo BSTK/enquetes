6913df4fabb41ee8708c97b06a9e7f56a6a6417f
Bruno Luz <bsilva.se@gmail.com>
2021-02-28 18:36:38 -0300

    main - Refatorando factory: LoginPage

--- lib/main/factories/pages/login/login_page_factory.dart
+++ lib/main/factories/pages/login/login_page_factory.dart
@@ -1,33 +1,6 @@
-import 'package:enquetes/data/usecases/usecases.dart';
-import 'package:enquetes/infra/infra.dart';
-import 'package:enquetes/presentation/presentation.dart';
 import 'package:enquetes/ui/pages/pages.dart';
-import 'package:enquetes/validation/validators/validators.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 
-Widget loginPage() {
-  const url = 'http://fordevs.herokuapp.com/api/login';
+import 'login.dart';
 
-  final httpClient = HttpAdapter(client: Client());
-
-  final autenticacaoRemota = AutenticarUsuarioRemoto(
-    httpClient: httpClient,
-    url: url
-  );
-
-  final validationComposite = ValidationComposite(
-      validators: [
-        EmailValidator(field: 'email'),
-        RequiredFieldValidation(field: 'email'),
-        RequiredFieldValidation(field: 'senha')
-      ]
-  );
-
-  final presenter = StreamLoginPresenter(
-    validation: validationComposite,
-    autenticacao: autenticacaoRemota
-  );
-
-  return LoginPage(presenter);
-}
+Widget loginPage() => LoginPage(loginPresenter());
